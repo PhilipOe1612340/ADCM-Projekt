05a0801418547c5d56d091ef6d26c0390547d788
Unknown <1612340@stud.hs-mannheim.de>
2018-01-23 17:33:38 +0100

    added Vue Material

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,8 +1,26 @@
 <template>
-  <div @click="test" class="hello">
+  <div class="hello">
     <h1>{{ msg }}</h1>
     <h2>Essential Links</h2>
     <h2>Ecosystem</h2>
+      <md-button class="md-accent md-raised" id="button" @click="test">Alert</md-button>
+      <md-card id="card" md-with-hover>
+        <md-ripple>
+          <md-card-header>
+            <div class="md-title">Card withakljsdhfkajsh hover effect</div>
+            <div class="md-subhead">It also have a ripple</div>
+          </md-card-header>
+
+          <md-card-content>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio itaque ea, nostrum odio. Dolores, sed accusantium quasi non.
+          </md-card-content>
+
+          <md-card-actions>
+            <md-button>Action</md-button>
+            <md-button>Action</md-button>
+          </md-card-actions>
+      </md-ripple>
+    </md-card>
   </div>
 </template>
 
@@ -22,10 +40,13 @@ export default {
 };
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 h1,
 h2 {
   font-weight: normal;
+}
+#card {
+  width: 700px;
+  margin-left: 500px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,10 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import VueMaterial from "vue-material";
+import "vue-material/dist/vue-material.min.css";
+
+Vue.use(VueMaterial);
 
 Vue.config.productionTip = true
 
@@ -13,3 +17,4 @@ new Vue({
   components: { App },
   template: '<App/>'
 })
+
