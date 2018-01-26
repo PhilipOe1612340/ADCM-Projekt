870389791d23b965c4fc4016f3527bbb56303acc
Unknown <1612340@stud.hs-mannheim.de>
2018-01-26 23:12:01 +0100

    integraded themes

--- src/main.js
+++ src/main.js
@@ -1,15 +1,14 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue';
-import App from './App';
-import router from './router';
-import store from "./store";
-import { sync } from "vuex-router-sync";
-import VueMaterial from "vue-material";
-import "vue-material/dist/vue-material.min.css";
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import store from "./store"
+import { sync } from "vuex-router-sync"
+import VueMaterial from "vue-material"
+import "vue-material/dist/vue-material.min.css"
+import "./theme.scss"
 
 
-Vue.use(VueMaterial);
+Vue.use(VueMaterial)
 
 Vue.config.productionTip = true
 
