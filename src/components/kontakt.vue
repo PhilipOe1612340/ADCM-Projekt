874bc5edf9a24560fcc0fe57b9f446106feb47b1
Unknown <1612340@stud.hs-mannheim.de>
2018-01-31 10:45:02 +0100

    responsive adress

--- src/components/kontakt.vue
+++ src/components/kontakt.vue
@@ -1,54 +1,72 @@
 <template>
   <div>
-   <form validate class="md-layout" @submit.prevent="validateUser">
-      <md-card class="md-layout-item md-size-50 md-small-size-100">
-        <md-card-header>
-          <div class="md-title">Kontakt</div>
-        </md-card-header>
+    <div class="md-layout md-gutter">
+      <div class="md-layout-item md-size-20 md-medium-hide"></div>
+      <div class="md-layout-item md-large-size-35 md-medium-size-45 md-small-size-95">
+        <form validate class="md-layout" @submit.prevent="validateUser">
+          <md-card class="md-layout-item">
+            <md-card-header>
+              <div class="md-title">Kontakt</div>
+            </md-card-header>
 
-        <md-card-content>
-          <div class="md-layout md-gutter">
-            <div class="md-layout-item md-small-size-100">
-              <md-field :class="getValidationClass('name')">
-                <label for="first-name">Name</label>
-                <md-input name="first-name" id="first-name" autocomplete="given-name" v-model="form.name" :disabled="sending" />
-                <span class="md-error" v-if="!$v.form.name.required">Ihr Name wird benötigt</span>
-                <span class="md-error" v-else-if="!$v.form.name.minlength">Invalid first name</span>
+            <md-card-content>
+              <div class="md-layout md-gutter">
+                <div class="md-layout-item md-small-size-100">
+                  <md-field :class="getValidationClass('name')">
+                    <label for="first-name">Name</label>
+                    <md-input name="first-name" id="first-name" autocomplete="given-name" v-model="form.name" :disabled="sending" />
+                    <span class="md-error" v-if="!$v.form.name.required">Ihr Name wird benötigt</span>
+                    <span class="md-error" v-else-if="!$v.form.name.minlength">Invalid first name</span>
+                  </md-field>
+                </div>
+              </div>
+
+              <md-field :class="getValidationClass('email')">
+                <label for="email">Email</label>
+                <md-input type="email" name="email" id="email" autocomplete="email" v-model="form.email" :disabled="sending" />
+                <span class="md-error" v-if="!$v.form.email.required">Ihre Email wird benötigt</span>
+                <span class="md-error" v-else-if="!$v.form.email.email">Invalid email</span>
               </md-field>
-            </div>
-          </div>
 
-          <md-field :class="getValidationClass('email')">
-            <label for="email">Email</label>
-            <md-input type="email" name="email" id="email" autocomplete="email" v-model="form.email" :disabled="sending" />
-            <span class="md-error" v-if="!$v.form.email.required">Ihre Email wird benötigt</span>
-            <span class="md-error" v-else-if="!$v.form.email.email">Invalid email</span>
-          </md-field>
+              <div class="md-layout md-gutter">
+                <div class="md-layout-item md-small-size-100">
+                  <md-field :class="getValidationClass('betreff')">
+                    <label for="first-name">Betreff</label>
+                    <md-input name="first-name" autocomplete="given-name" v-model="form.betreff" :disabled="sending" />
+                  </md-field>
+                </div>
+              </div>
 
-          <div class="md-layout md-gutter">
-            <div class="md-layout-item md-small-size-100">
-              <md-field :class="getValidationClass('betreff')">
-                <label for="first-name">Betreff</label>
-                <md-input name="first-name" autocomplete="given-name" v-model="form.betreff" :disabled="sending" />
+              <md-field>
+                <label>Ihre Mitteilung</label>
+                <md-textarea v-model="form.text"></md-textarea>
               </md-field>
-            </div>
-          </div>
+            </md-card-content>
 
-          <md-field>
-            <label>Ihre Mitteilung</label>
-            <md-textarea v-model="form.text"></md-textarea>
-          </md-field>
-        </md-card-content>
+            <md-progress-bar md-mode="indeterminate" v-if="sending" />
 
-        <md-progress-bar md-mode="indeterminate" v-if="sending" />
+            <md-card-actions>
+              <md-button type="submit" class="md-primary" @click="validateUser" :disabled="sending">Absenden</md-button>
+            </md-card-actions>
+          </md-card>
 
-        <md-card-actions>
-          <md-button type="submit" class="md-primary" @click="validateUser" :disabled="sending">Absenden</md-button>
-        </md-card-actions>
-      </md-card>
-
-      <md-snackbar :md-active.sync="userSaved">Ihre Nachricht wurde zugestellt</md-snackbar>
-    </form>
+          <md-snackbar :md-active.sync="userSaved">Ihre Nachricht wurde zugestellt</md-snackbar>
+        </form>
+      </div>
+      <div class="md-layout-item md-large-size-35 md-medium-size-45 md-small-size-95">
+        Agentur_dcm<br/>
+        Agentur für Design und Fotographie</p>
+        <p>dcm Galerie<br/>
+        Schorndorfer Str. 33<br/>
+        D – 70736 Fellbach</p>
+        <p>Postanschrift<br/>
+        Fellbacher Str. 109<br/>
+        D – 70736 Fellbach</p>
+        <p>Tel	+49 (0)711 300 36 91<br/> </p>
+        Email <a href="mailto:info@agentur-dcm.de">info@agentur-dcm.de</a>
+      </div>
+      <div class="md-layout-item md-large-size-5 md-medium-hide"></div>
+    </div>
   </div>
 </template>
 
@@ -138,5 +156,15 @@ export default {
   top: 0;
   right: 0;
   left: 0;
+}
+.md-layout-item {
+  overflow-x: hidden;
+  margin: 0 10px 10px 10px;
+
+  &:after {
+    width: 100%;
+    display: block;
+    background: md-get-palette-color(red, 200);
+  }
 }
 </style>
