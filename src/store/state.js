c43181cc712b4b6ecca39b699e57805c55c6c9ec
Unknown <1612340@stud.hs-mannheim.de>
2018-02-01 16:39:42 +0100

    no Api version

--- src/store/state.js
+++ src/store/state.js
@@ -3,7 +3,28 @@ export default {
     error: null,
     loading: false
   },
-  news: [],
+  news: [
+    {
+      title: "neue Webseite",
+      body: "Hier könnte ihr Artikel stehen",
+      date: "2018-02-01T15:27:15.779Z"
+    },
+    {
+      title: "neue Webseite",
+      body: "Hier könnte ihr Artikel stehen",
+      date: "2018-03-01T15:27:16.230Z"
+    },
+    {
+      title: "neue Webseite",
+      body: "Hier könnte ihr Artikel stehen",
+      date: "2018-04-01T15:27:25.275Z"
+    },
+    {
+      title: "neue Webseite",
+      body: "Hier könnte ihr Artikel stehen",
+      date: "2018-05-01T15:27:25.719Z"
+    }
+  ],
   settings: {
     serverIp: "http://localhost:1880"
   }
