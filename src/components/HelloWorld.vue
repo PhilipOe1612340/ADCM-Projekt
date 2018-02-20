1730829a64c9997e131d65c9567e17ae93587815
Philip Oesterlin <1612340@stud.hs-mannheim.de>
2018-02-20 15:24:36 +0100

    cleanup

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,69 +1,23 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <h2>Ecosystem</h2>
-      <md-button class="md-accent md-raised" id="button" @click="test">Alert</md-button>
-      <md-card id="card" md-with-hover>
-        <md-ripple>
-          <md-card-header>
-            <div class="md-title">Card withakljsdhfkajsh hover effect</div>
-            <div class="md-subhead">It also have a ripple</div>
-          </md-card-header>
-
-          <md-card-content>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio itaque ea, nostrum odio. Dolores, sed accusantium quasi non.
-          </md-card-content>
-
-          <md-card-actions>
-            <md-button>Action</md-button>
-            <md-button>Action</md-button>
-          </md-card-actions>
-      </md-ripple>
-    </md-card>
-    <div id="input">
-      store test
-      <md-input autocomplete="given-name" v-model="error" />
-    </div>
+  <div>
+    
   </div>
 </template>
 
 <script>
 export default {
-  name: "HelloWorld",
+  name: "comp",
   data() {
     return {
-      msg: "Welcome to Your Vue.js App"
+      msg: null
     };
   },
   methods: {
-    test() {
-      this.msg += 5;
-    }
   },
   computed: {
-    error: {
-      get() {
-        return this.$store.getters.getError;
-      },
-      set(val) {
-        this.$store.commit("error", val);
-      }
-    }
   }
 };
 </script>
 
 <style scoped>
-h1,
-h2 {
-  font-weight: normal;
-}
-#card {
-  width: 700px;
-  margin-left: 500px;
-}
-#input {
-  margin: 30px;
-}
 </style>
